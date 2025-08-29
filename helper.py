99bf65b3e8749c555f50448cba85139c87affce4
Diparghya-Mallik <mallikdiparghya@gmail.com>
2025-08-30 01:12:06 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,9 +3,9 @@ import preprocessor,helper
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.sidebar.title("Whatsapp Chat Analyzer")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
+st.sidebar.title("Whatsapp Chat Analyzer")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
